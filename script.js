89334a4c506a16251f732ab9f550003988f1dbae
Karol <karol.gratus@gmail.com>
2017-11-08 14:50:00 +0100

    new attemp to arrows

--- script.js
+++ script.js
@@ -4,22 +4,24 @@ const moveFirstSlide = function() {
   let firstItem = carouselList.find("li:last");
   let lastItem = carouselList.find("li:first");
   lastItem.before(firstItem)
-  carouselList.css({marginLeft:0});
+  carouselList.css({marginLeft:-400});
 };
 
-const carousel = () => carouselList.animate({'marginLeft': 400}, 500, moveFirstSlide);
-
+const carousel = function(direction="right") {
+  carouselList.animate({'marginLeft': 0}, 500, moveFirstSlide);
+  console.log(carousel);
+}
 // const moveToLeft = () => carouselList.animate({'marginRight':400}, 500, moveFirstSlide);
 
-$('.arrow-right').click(function(event) {
+$(".arrow-right").click(function(event) {
   event.preventDefault();
   carousel();
 })
 
-// $('.arrow-left').click(function(event) {
-//   event.preventDefault();
-//   moveToLeft();
-// })
+$(".arrow-left").click(function(event) {
+  event.preventDefault();
+  carousel("left");
+})
 
 
 setInterval(carousel, 3000);
