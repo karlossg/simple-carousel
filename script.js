00a807cbed2b50c5fd0f7eb52d8e9359e5705ef7
Karol <karol.gratus@gmail.com>
2017-11-10 11:17:33 +0100

    arrows done

--- script.js
+++ script.js
@@ -4,30 +4,66 @@ const carouselList = $("#carousel ul");
 const dotList = $("#dotsList li");
 
 
-function moveFirstSlide(side) {
-  let firstItem = carouselList.find("li:first");
-  let lastItem = carouselList.find("li:last");
-  if (side === "right") {
-  lastItem.after(firstItem);
-  carouselList.css({marginLeft: 0});
-  activeDot();
-  } else {
-    firstItem.before(lastItem)
-    carouselList.css({marginLeft:-400});
-    activeDot();
-  }
+// function moveFirstSlide(side) {
+//   let firstItem = carouselList.find("li:first");
+//   let lastItem = carouselList.find("li:last");
+//   if (side === "right") {
+//   lastItem.after(firstItem);
+//   carouselList.css({marginLeft: 0});
+//   activeDot();
+//   } else {
+//     lastItem.css({marginLeft:0});
+//     firstItem.before(lastItem)
+
+//     activeDot();
+//   }
+// };
+
+// function carousel(margin, side) {
+//   carouselList.animate({'marginLeft': margin}, 500, () => moveFirstSlide(side));
+// }
+
+function removeActiveDot() {
+  $('#dotsList .active').removeClass("active");;
+}
+
+
+function addActiveDot() {
+  let $activeItem = $('#photo-list li:first');
+  let $activeDotId = $activeItem.attr("id");
+  $("#dot" + $activeDotId).addClass("active");
+}
+
+
+function moveLeft() {
+  removeActiveDot();
+  setTimeout(addActiveDot, 1000);
+  const $last = $('#photo-list li:last');
+  $last.remove().css({
+    'margin-left': '-400px'
+  });
+  $('#photo-list li:first').before($last);
+  $last.animate({
+    'margin-left': '0px'
+  }, 800);
+  
 };
 
-function carousel(margin, side) {
-  carouselList.animate({'marginLeft': margin}, 500, moveFirstSlide(side));
-}
 
-function activeDot() {
-  let activeItem = carouselList.find("li:first");
-  let activeDotId = activeItem.attr("id");
-  $("#dotsList li").removeClass("active");
-  $("#dot" + activeDotId).addClass("active");
-}
+function moveRight() {
+  removeActiveDot();
+  setTimeout(addActiveDot, 1000);
+  const $first = $('#photo-list li:first');
+  $first.animate({
+    'margin-left': '-410px'
+  }, 800, function() {
+    $first.remove().css({
+      'margin-left': '0px'
+    });
+    $('#photo-list li:last').after($first);
+  });
+  
+};
 
 // function showSlide(id) {
 //   $(".dot").click(function(e) {
@@ -39,11 +75,12 @@ function activeDot() {
 //   })
 // }
 
-function moveArrow(margin, side) {
-  event.preventDefault();
-  carousel(margin, side);
-}
+// function moveArrow(margin, side) {
+//   event.preventDefault();
+//   carousel(margin, side);
+// }
+$('#right').on('click', moveRight);
 
-// setInterval(() => carousel(-400, "right"), 3000);
+$('#left').on('click', moveLeft);
 
-
+setInterval(moveRight, 4000);
